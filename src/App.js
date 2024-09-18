8b7f55d33847de38cfe171fcdd70d497eabdaece
vinny <doanvinhnguyen0711@gmail.com>
2024-09-18 09:45:32 +0700

    fe new

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react'
+import './asset/scss/App.scss'
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='header'></div>
+      <div className='body'>
+        <div className='body__left'></div>
+        <div className='center'>
+          <div className='circle1'>
+          </div>
+        </div>
+        <div className='body__right'></div>
+      </div>
+      <div className='footer'></div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
